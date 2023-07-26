c957807945cd78998b80844273e1adc2fd11e335
Aztro-dev <davidperezcastellanos@gmail.com>
2023-07-25 20:41:54 -0500

    Got rid of ds_store and duplicate files, as well as added DS_Store files into the .gitignore

--- src/drawing 2.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) mod canvas;
-pub(crate) mod color;
--- src/geometry 2.rs
+++
@@ -1,9 +0,0 @@
-
-
-pub(crate) mod alignment;
-pub(crate) mod decoration;
-pub(crate) mod rect;
-pub(crate) mod screen_side;
-pub(crate) mod size_requirements;
-
-
